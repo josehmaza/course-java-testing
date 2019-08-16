f9278e25204d2416cbd9180f762862f0948c8878
josehmaza <josehmaza@gmail.com>
2019-08-15 23:04:03 -0500

    Creacion de la base de datos y tests de integracion

--- src/main/java/com/platzi/javatests/movies/model/Movie.java
+++ src/main/java/com/platzi/javatests/movies/model/Movie.java
@@ -1,11 +1,29 @@
 package com.platzi.javatests.movies.model;
 
+import java.util.Objects;
+
 public class Movie {
 
     private Integer id;
     private String name;
     private int minutes;
     private Genre genre;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return minutes == movie.minutes &&
+                Objects.equals(id, movie.id) &&
+                Objects.equals(name, movie.name) &&
+                genre == movie.genre;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, minutes, genre);
+    }
 
     public Movie(String name, int minutes, Genre genre) {
         this(null, name, minutes, genre);
